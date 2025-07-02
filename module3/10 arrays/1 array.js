f49ace17e1d02b62ba15dbdd13352f0d3f9d968e
ashreekar <ashreekar767@gmail.com>
2025-07-02 19:13:06 +0530

    ss july 2 25

---
+++ module3/10 arrays/1 array.js
@@ -0,0 +1,27 @@
+// Using array literals
+
+const colours=["red","blue","green","notcol","white"]
+console.log(colours[0])
+console.log(colours[1])
+console.log(colours[3])
+
+colours[3]="black";
+console.log(colours[3])
+
+console.log(colours)
+
+colours[5]="pink"
+console.log(colours)
+// from an empty array
+
+const fruit=[]
+
+fruit[0]="Apple"
+fruit[1]="Guava"
+console.log(fruit)
+console.log(fruit[0])
+console.log(fruit[1])
+
+// using new keyword
+const duck=new Array("duck1","duck2","duck3")
+console.log(duck)
