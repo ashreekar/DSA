328f9b05772a4852fbbd6113b978af38aa010a04
ashreekar <ashreekar767@gmail.com>
2025-07-05 19:16:42 +0530

    jul525

--- module3/ISP_Prac/prac4.js
+++ module3/ISP_Prac/prac4.js
@@ -3,3 +3,23 @@ let arr=['ananas','apple','banana','Honey','Bamboo','Aaple']
 let srtd=arr.sort()
 console.log(arr)
 console.log(srtd)
+
+var x=5;
+(function(){
+    var x=10
+    console.log(x)
+})()
+console.log(x)
+
+const number=[1,2,3,4,5]
+const [f,s,...r]=number
+console.log(f,s,r)
+
+function sum(...args){
+    return args.reduce((acc,curr)=>acc+curr,0)
+}
+console.log(sum(...number))
+
+let ss='hello'
+let char=[...ss]
+console.log(char)
